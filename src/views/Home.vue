ea605f5121060bae70ededf38924fc41f8302e4e
Prasanna Munnangi <pmunnangi@macbookpro-5104.ad.infotechfl.com>
2019-04-30 17:16:46 -0400

    initial vuejs commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HomeHero />
+    <HomeDetails />
+    <HomePlans />
+  </div>
+</template>
+
+<script>
+import HomeHero from '../components/HomeHero'
+import HomeDetails from '../components/HomeDetails'
+import HomePlans from '../components/HomePlans'
+
+export default {
+  name: 'home',
+  components: {
+    HomeHero,
+    HomeDetails,
+    HomePlans
+  }
+}
+</script>
